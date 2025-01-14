503d439817fc4e2f1eb5ee3d87cdf8d443020761
Manel-ken <kennouchemanel@gmail.com>
2025-01-14 13:20:52 +0100

    Update ansible playbook and configs files

--- ansible/setup_ansible.sh
+++ ansible/setup_ansible.sh
@@ -3,19 +3,47 @@
 #cd Ansible
 
 # ---------------------- Set up SSH Passwordless communication between Ansible controller and remote nodes ------
-# Step 1: Update the system then Install ssh password and ansible
-echo -e "\n-Step 1: Update the system..."
-sudo apt update
-echo -e "\n- Installing sshpass..."
-sudo apt install ansible sshpass -y
+# Step 1: Check if Ansible is installed, if not, install it
+echo -e "\n-Step 1: Checking if Ansible is installed..."
+if ! command -v ansible &> /dev/null
+then
+    echo -e "\n-Ansible is not installed. Installing Ansible and sshpass..."
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible sshpass -y
+else
+    echo -e "\n-Ansible is already installed."
+fi
 
-# Step 2:  Disable host key checking
-echo -e "\n-Step 2: Disable host key checking..."
-grep -q -F 'host_key_checking = false' /etc/ansible/ansible.cfg  || sudo sed -i '/^\[defaults\]/a host_key_checking = false' /etc/ansible/ansible.cfg
 
-# Step 3: Generate SSH keys
+# Step 2: Create ansible.conf
+echo -e "\n-Step 2: Creating ansible.conf..."
+cat <<EOL > ansible.cfg
+[defaults]
+inventory = inventory
+remote_user = vagrant
+host_key_checking = False
+EOL
+
+# Step 3: Check if SSH keys exist, and generate if not
 echo -e "\n-Step 3: Generating SSH keys..."
-ssh-keygen -t rsa -f  /home/vagrant/.ssh/id_rsa  -N ''
+
+# Check if the private key already exists
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+  # If the file does not exist, generate the SSH key pair
+  ssh-keygen -t rsa -f "$HOME/.ssh/id_rsa" -N ''
+  echo "- SSH keys generated successfully."
+else
+  # If the file exists, print a message
+  echo "- SSH keys already exist."
+fi
+
+#sudo useradd -m -s /bin/bash vagrant
+#echo "vagrant:manel159" | sudo chpasswd
+#su vagrant #(enter password)
+#mkdir -p /home/vagrant/.ssh
+#chmod 700 /home/vagrant/.ssh
 
 # Step 4: Exchange Ansible ssh public key with remote nodes to enable SSH Passwordless communication
 echo -e "\n-Step 4: Exchanging Ansible ssh public key with remote nodes..."
@@ -23,12 +51,8 @@ ansible-playbook exchange-keys-playbook.yml -i inventory -u vagrant --ask-pass
 
 # Step 5: Testing connectivity to the servers
 echo -e "\n-Step 5: Testing connectivity to the remote nodes..."
-ansible all -m ping -i inventory
+ansible all -m ping -i inventory -u vagrant
 
-#--------------------  Packages installation in k8s nodes and mec  -----------------------
-# Step 6: Run playbook which Installing Necessary Packages on Kubernetes Nodes
-#echo -e "\n-Step 6: Installing Necessary Packages on Kubernetes Nodes..."
-#ansible-playbook package_instal_compose_node.yml  -i inventory -u vagrant
 
 
 
